8938705269e7f633cd9fc68ae46729556bf8a0f5
Mahdia-123 <160815889+Mahdia-123@users.noreply.github.com>
2025-05-16 18:08:05 +0430

    added the basic javascript to my project

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,23 @@
+function updateTime() {
+  let sydneyElement = document.querySelector("#sydney");
+  let sydneyDateElement = sydneyElement.querySelector(".date");
+  let sydneyTimeElement = sydneyElement.querySelector(".time");
+  let sydneyTime = moment().tz("Austrilia/Sydney");
 
+  sydneyDateElement.innerHTML = sydneyTime.format("MMMM Do YYYY");
+  sydneyTimeElement.innerHTML = sydneyTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+
+  let torontoElement = document.querySelector("#toronto");
+  let torontoDateElement = torontoElement.querySelector(".date");
+  let torontoTimeElement = torontoElement.querySelector(".time");
+  let torontoTime = moment().tz("America/Toronto");
+
+  torontoDateElement.innerHTML = torontoTime.format("MMMM Do YYYY");
+  torontoTimeElement.innerHTML = torontoTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+}
+updateTime();
+setInterval(updateTime, 1000);
